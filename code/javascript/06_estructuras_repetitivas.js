c33eab1104132bca5b87d1566e396f6a1dc97230
Miguel Palacios <31173603+mpalaciosg1@users.noreply.github.com>
2020-09-23 20:07:23 -0500

    estructurarep

--- code/javascript/06_estructuras_repetitivas.js
+++ code/javascript/06_estructuras_repetitivas.js
@@ -1,12 +1,31 @@
-var estudiante = {
+//se puede usar : como un igual??, let que diferencia tiene de var??
+let estudiante = {
     nombres: "Migue Alexander",
     apellidos: "palacios guzman",
     correo: "asd@asd.com",
     peso: 150,
 }
 
-var meta = estudiante.peso - 10
+//CONST para constantes, se usan en mayusculas por convencion
+const INCREMENTAR_PESO = 3
+const DECREMENTAR_PESO = 2
+const META = estudiante.peso - 10
 
-while (estudiante.peso > meta) {
-    
+const aumentarPeso = (objeto) => objeto.peso += INCREMENTAR_PESO
+const aumentarPeso = (objeto) => objeto.peso -= DECREMENTAR_PESO
+
+const comeMucho = () => Math.random() <0.6
+const realizaDeporte = () => Math.random() < 0.3  
+
+console.log(`Al inicio del año ${estudiante.nombres} pesa ${estudiante.peso}`)
+
+while (estudiante.peso > META) {
+    if(comeMucho()){
+        aumentarPeso(estudiante)
+    }
+    if (realizaDeporte()) {
+        disminuyePeso(estudiante)
+    }
 }
+
+console.log(`Al final del año ${estudiante.nombres} pesa ${estudiante.peso}`)
